a8599550dd38c63fe15af9a04d715ca951b90272
tony <chenjun_tony@126.com>
2023-11-18 22:39:11 +0800

    init project

---
+++ src/main/java/com/tony/springboot/demo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tony.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
